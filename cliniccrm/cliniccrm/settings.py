0f31ac7263ed5158dbba5b6a80aaf7ee402a2fe4
faxriyorarslon1 <faxriyorarslon1@gmail.com>
2022-06-25 19:48:04 +0500

    allowed hosts *

--- cliniccrm/cliniccrm/settings.py
+++ cliniccrm/cliniccrm/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-uobacee1#9#o4mypi5(u+syvz!xdnpbgj_hb@x6_+7=d&1g^r8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
